d92dd851a84ef5a89f8a453905868daddfdd8a1e
Bong <qhdgkdbs@gmail.com>
2022-06-20 01:55:32 +0900

    docs: template added

---
+++ template.py
@@ -0,0 +1,15 @@
+import time
+
+def func_name():
+    result = 0
+    # solve here
+
+    return result
+
+
+if __name__ == '__main__':
+    start = time.time()
+    print(func_name())
+    print(f"{time.time() - start:.10f} sec")
+
+
